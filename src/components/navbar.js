91c0632bdcc5973a8731851a6ba6d714c26e10f7
yasira <yasirayusif@gmail.com>
2022-07-13 15:42:07 -0700

    styled navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,3 +1,5 @@
+import "./navbar.css";
+
 const Navbar = () => {
 
     return (
@@ -6,7 +8,7 @@ const Navbar = () => {
       
             <header>
                 
-                <h1>WiTech</h1>
+                <h1 id="logo">COL'CACCHIO</h1>
                 
                 <nav>
                     
@@ -14,19 +16,31 @@ const Navbar = () => {
                         
                         <li><a href="#">Home</a></li>
                         
-                        <li><a href="#">About</a></li>
+                        <li><a href="#">MENU</a></li>
                         
-                        <li><a href="#">Team</a></li>
+                        <li><a href="#">RESTATURANTS</a></li>
                         
-                        <li><a href="#">Service</a></li>
+                        <li><a href="#">PROMOTIONS</a></li>
                         
-                        <li><a href="#">Portfolio</a></li>
+                        <li><a href="#">FRANCHISE</a></li>
+
+                        <li><a href="#">OPPORTUNITIES</a></li>
                         
                         <li><a href="#">Contact</a></li>
                         
                     </ul>
                     
                 </nav>
+
+                <div className = " socialMediaLinks">
+                        
+                    <i className="fab fa-facebook-f icon"></i>
+
+                    <i className="fab fa-instagram icon"></i>
+
+                    <i className="fab fa-twitter icon"></i>
+
+                </div>
                 
             </header>
             
