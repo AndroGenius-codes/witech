51e964dfe64bb48766eed63931694014cfbaef60
yasira <yasirayusif@gmail.com>
2022-07-12 20:03:04 -0700

    started with navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,39 @@
+const Navbar = () => {
+
+    return (
+      
+        <>
+      
+            <header>
+                
+                <h1>WiTech</h1>
+                
+                <nav>
+                    
+                    <ul>
+                        
+                        <li><a href="#">Home</a></li>
+                        
+                        <li><a href="#">About</a></li>
+                        
+                        <li><a href="#">Team</a></li>
+                        
+                        <li><a href="#">Service</a></li>
+                        
+                        <li><a href="#">Portfolio</a></li>
+                        
+                        <li><a href="#">Contact</a></li>
+                        
+                    </ul>
+                    
+                </nav>
+                
+            </header>
+            
+        </>
+        
+    )
+    
+}
+
+export default Navbar
